5e3ddeff333e73ea907d0021668926643b570683
Ray <lighthouseinmind@yeah.net>
2025-08-22 17:49:15 +0800

    Update plan-template.service.ts

--- src/plan/platform/plan-template/plan-template.service.ts
+++ src/plan/platform/plan-template/plan-template.service.ts
@@ -2,7 +2,7 @@
  * @Author: Ray lighthouseinmind@yeah.net
  * @Date: 2025-07-08 14:59:59
  * @LastEditors: Ray lighthouseinmind@yeah.net
- * @LastEditTime: 2025-08-22 17:46:30
+ * @LastEditTime: 2025-08-22 17:48:17
  * @FilePath: /card-backend/src/card/pdf-print-info/pdf-print-info.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,6 +28,17 @@ export class PlatfromPlanTemplateService {
     return this.prismaService.planTemplate.findMany({
       orderBy: {
         id: 'desc',
+      },
+      include: {
+        PlanTemplateAndTaskGroupRelation: {
+          include: {
+            platform_task_group: {
+              select: {
+                name: true
+              }
+            }
+          }
+        }
       },
       where: filter,
       skip: offset,
