61dadef0161fdb30802a579096a35de822af57d1
Reflection <lighthouseinmind@yeah.net>
2026-01-14 22:47:06 +0800

    修复心跳接口

--- src/plan/user/task/task.controller.ts
+++ src/plan/user/task/task.controller.ts
@@ -2,7 +2,7 @@
  * @Author: Ray lighthouseinmind@yeah.net
  * @Date: 2025-07-08 14:59:59
  * @LastEditors: Reflection lighthouseinmind@yeah.net
- * @LastEditTime: 2025-12-24 00:32:32
+ * @LastEditTime: 2026-01-14 22:45:57
  * @FilePath: /card-auto-planning/src/plan/platform/plan-template/plan-template.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,6 +30,7 @@ export class UserTaskController {
 
   //  心跳接口
   @Put('heartbeat/:id')
+  @UseGuards(JwtAuthGuard, RoleGuard('miniUser'))
   async heartbeat(@Req() req, @Param('id') id: number) {
     const userId = req.user.accountId;
     const res = await this.service.heartbeat(id, userId);
