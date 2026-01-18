1a7ba4c9f86beefe60656e3c896a53fb4fd888d4
Reflection <lighthouseinmind@yeah.net>
2026-01-18 23:05:51 +0800

    补全字段

--- src/plan/user/plan/plan.service.ts
+++ src/plan/user/plan/plan.service.ts
@@ -2,7 +2,7 @@
  * @Author: Ray lighthouseinmind@yeah.net
  * @Date: 2025-07-08 14:59:59
  * @LastEditors: Reflection lighthouseinmind@yeah.net
- * @LastEditTime: 2025-12-24 00:21:05
+ * @LastEditTime: 2026-01-18 23:01:43
  * @FilePath: /card-backend/src/card/pdf-print-info/pdf-print-info.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -45,6 +45,7 @@ export class UserPlanService {
             status: true,
             task: {
               select: {
+                id: true,
                 name: true,
                 task_group_id: true,
                 background: true,
