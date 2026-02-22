61e7ff49825f59eb8bc8cf3c0003f7c80d6975de
Reflection <lighthouseinmind@yeah.net>
2026-02-22 18:16:53 +0800

    增加复制平台任务集接口

--- src/admin/user/task-group/task-group.controller.ts
+++ src/admin/user/task-group/task-group.controller.ts
@@ -2,7 +2,7 @@
  * @Author: Ray lighthouseinmind@yeah.net
  * @Date: 2025-07-08 14:59:59
  * @LastEditors: Reflection lighthouseinmind@yeah.net
- * @LastEditTime: 2026-02-11 23:30:45
+ * @LastEditTime: 2026-02-22 18:16:09
  * @FilePath: /card-auto-planning/src/plan/platform/plan-template/plan-template.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,6 +27,31 @@ export class AdminUserTaskGroupController {
     }; 
   }
 
+  @Put(':id')
+  @UseGuards(JwtAuthGuard, RoleGuard('backUser'))
+  async update(@Req() req, @Param('id') id: number, @Body('mini_user_id') userId: number, @Body('name') name: string, @Body('background') background: string) {
+    const res = await this.service.updateTaskGroup(Number(id), name, background, userId);
+    return {
+      code: HttpStatus.OK,
+      data: res,
+      res: '成功',
+    };
+  }
+
+  /** 复制平台任务集：传入新名字和要复制的平台任务集 id，创建新任务集并复制其下所有任务 */
+  @Post('copy-from-platform')
+  @UseGuards(JwtAuthGuard, RoleGuard('backUser'))
+  async copyFromPlatform(
+    @Body('name') name: string,
+    @Body('source_task_group_id') sourceTaskGroupId: number,
+  ) {
+    const data = await this.service.copyFromPlatformTaskGroup(name, Number(sourceTaskGroupId));
+    return {
+      code: HttpStatus.CREATED,
+      data,
+      res: '成功',
+    };
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard, RoleGuard('backUser'))
@@ -37,16 +62,5 @@ export class AdminUserTaskGroupController {
       data: res,
       res: '成功',
     }; 
-  }
-
-  @Put(':id')
-  @UseGuards(JwtAuthGuard, RoleGuard('backUser'))
-  async update(@Req() req, @Param('id') id: number, @Body('mini_user_id') userId: number, @Body('name') name: string, @Body('background') background: string) {
-    const res = await this.service.updateTaskGroup(Number(id), name, background, userId);
-    return {
-      code: HttpStatus.OK,
-      data: res,
-      res: '成功',
-    };
   }
 }
--- src/admin/user/task-group/task-group.service.ts
+++ src/admin/user/task-group/task-group.service.ts
@@ -56,4 +56,67 @@ export class AdminUserTaskGroupService {
       },
     });
   }
+
+  /**
+   * 复制平台任务集：按 sourceTaskGroupId 取平台任务集及其下任务，创建新任务集（使用传入 name），并复制所有任务到新任务集下
+   */
+  async copyFromPlatformTaskGroup(name: string, sourceTaskGroupId: number) {
+    const source = await this.prismaService.platformTaskGroup.findUnique({
+      where: { id: sourceTaskGroupId },
+      include: {
+        PlatformTaskGroupAndTaskRelation: {
+          include: { platform_task: true },
+          orderBy: { group_sort: 'asc' },
+        },
+      },
+    });
+    if (!source) {
+      throw new HttpException('源任务集不存在', HttpStatus.NOT_FOUND);
+    }
+
+    return this.prismaService.$transaction(async (tx) => {
+      const newGroup = await tx.platformTaskGroup.create({
+        data: {
+          name,
+          background: source.background,
+        },
+      });
+
+      for (const rel of source.PlatformTaskGroupAndTaskRelation) {
+        const t = rel.platform_task;
+        const newTask = await tx.platformTask.create({
+          data: {
+            name: t.name,
+            preset_task_tag_id: t.preset_task_tag_id,
+            suggested_time_start: t.suggested_time_start,
+            suggested_time_end: t.suggested_time_end,
+            remark: t.remark,
+            annex_type: t.annex_type,
+            annex: t.annex,
+            timing_type: t.timing_type,
+            occupation_time: t.occupation_time,
+          },
+        });
+        await tx.platformTaskGroupAndTaskRelation.create({
+          data: {
+            platform_task_group_id: newGroup.id,
+            platform_task_id: newTask.id,
+            priority: rel.priority,
+            group_sort: rel.group_sort,
+            can_divisible: rel.can_divisible,
+          },
+        });
+      }
+
+      return tx.platformTaskGroup.findUnique({
+        where: { id: newGroup.id },
+        include: {
+          PlatformTaskGroupAndTaskRelation: {
+            include: { platform_task: true },
+            orderBy: { group_sort: 'asc' },
+          },
+        },
+      });
+    });
+  }
 }
